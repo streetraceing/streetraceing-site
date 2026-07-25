6ed63af27a3756d1c593eb2d2b83db2d7ad5aca9
streetraceing <voltzxt@vk.com>
2026-07-25 19:41:59 +0300

    chore: add typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
+// @ts-check
+
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import tseslint from 'typescript-eslint';
 
 const eslintConfig = defineConfig([
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    extends: [tseslint.configs.strict],
+  },
   ...nextVitals,
   ...nextTs,
   globalIgnores([
